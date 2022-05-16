d3ab1077413313f28a979341daf1b346ecffac93
Jeferson Arango <jeferson.arango@ucaldas.edu.co>
2022-05-16 17:43:24 -0500

    Enrutamiento y componentes de la plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './public/errors/error404/error404.component';
+import { HomeComponent } from './public/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: '**',
+    component: Error404Component,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/template/header/header.component';
+import { MenuComponent } from './public/template/menu/menu.component';
+import { FooterComponent } from './public/template/footer/footer.component';
+import { HomeComponent } from './public/home/home.component';
+import { Error404Component } from './public/errors/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
